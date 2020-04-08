0a36ef19a2d21d1b22160125970092c4b15ffe7d
Nicolas Felix <nicolas.felix@fhinck.com>
2020-04-08 09:45:56 -0300

    cleaning repo

--- src/app/main/example/ExampleConfig.js
+++ src/app/main/example/ExampleConfig.js
@@ -1,12 +1,5 @@
-import i18next from 'i18next';
 import Example from './Example';
-import en from './i18n/en';
-import tr from './i18n/tr';
-import ar from './i18n/ar';
 
-i18next.addResourceBundle('en', 'examplePage', en);
-i18next.addResourceBundle('tr', 'examplePage', tr);
-i18next.addResourceBundle('ar', 'examplePage', ar);
 
 const ExampleConfig = {
 	settings: {
